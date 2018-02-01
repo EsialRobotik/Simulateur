9f3f9d7c4179cfc9ec07e405c2839d9d43b8b751
clement-parisot <clement.parisot@telecomnancy.net>
2018-02-01 20:30:26 +0100

    libgdx bootstrap

---
+++ simulateur/desktop/src/esialrobotik/simulateur/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package esialrobotik.simulateur.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import esialrobotik.simulateur.Simulateur;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Simulateur(), config);
+	}
+}
