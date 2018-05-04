9c08befae282be61e3a6b9d16f19d619944b5a7b
Clement Parisot <clement.parisot@uni.lu>
2018-05-04 22:50:38 +0200

    Fix asserv in position
    Signed-off-by: Clement Parisot <clement.parisot@uni.lu>

--- simulateur/core/src/esialrobotik/simulateur/bullet/object/Robot.java
+++ simulateur/core/src/esialrobotik/simulateur/bullet/object/Robot.java
@@ -22,19 +22,18 @@ public class Robot extends BulletObject{
 	private static final float codOffsetY = 0f;
 	private static final float codOffsetX = 0f;
 	private static final float codOffsetZ = 0f;
-	private Vector3 [] lastCod = {new Vector3(), new Vector3()}; 
-	private float [] tick = {0f, 0f};
-	private float [] consigne = {60f, 60f};
-	private float [] errorCod = {0f, 0f};
-	private float [] sumErrorCod = {0f, 0f};
-	private float [] lastErrorCod = {0f, 0f};
-	private float [] commandeCod = {0f, 0f};
+	private float tick = 0f;
+	private float consigne = 30f;
+	private float errorCod = 0f;
+	private float sumErrorCod = 0f;
+	private float lastErrorCod = 0f;
+	private float commandeCod = 0f;
 	private int stable = 0;
-	private final float Kp = 10f;
+	private final float Kp = 100f;
 	private final float Ki = 0.001f;
-	private final float Kd = 100f;
-	private final float erreurAdmissible = 0.001f;
-	private final int asservStable = 300;
+	private final float Kd = 50f;
+	private final float erreurAdmissible = 0.0001f;
+	private final int asservStable = 1;
 	private static int initFrames = 50; // Wait for 10 frames before doing anything
 
 	public Robot() {
@@ -47,7 +46,8 @@ public class Robot extends BulletObject{
 						FloatAttribute.createShininess(16f)),
 				Usage.Position | Usage.Normal);
 		ModelInstance instance = new ModelInstance(model);
-		instance.transform.trn(size/2f, size/2f, size/2f);
+		instance.transform.trn(100f+size/2f, size/2f, 50f+size/2f);
+		instance.transform.rotate(Vector3.Y, 45);
 		btBoxShape boxShape = new btBoxShape(new Vector3(size/2f, size/2f, size/2f));
 		boxShape.calculateLocalInertia(1f, tempVector);
 		btRigidBodyConstructionInfo boxInfo = new btRigidBodyConstructionInfo(1f, null, boxShape, tempVector);
@@ -59,46 +59,25 @@ public class Robot extends BulletObject{
 		addInstance(instance, boxShape, boxInfo, boxMotionState, boxBody);
 	}
 	
-	private Vector3 positionCodeuse(Vector3 positionRobot, boolean left) {
-		float offset = left ? entraxeCod : -entraxeCod;
-		Vector3 pos = new Vector3(offset+codOffsetX, codOffsetY, codOffsetZ);
-		float orientation = super.bodies.get(0).getOrientation().getAngle();
-		pos.rotate(Vector3.Y, orientation);
-		pos.add(positionRobot);
-		return pos;
-	}
-	
 	private void updatePositions() {
-		lastPosition = super.bodies.get(0).getCenterOfMassPosition();
-		lastCod[0] = positionCodeuse(lastPosition, false);
-		lastCod[1] = positionCodeuse(lastPosition, true);
+		lastPosition = super.bodies.get(0).getCenterOfMassPosition().cpy();
 	}
 	
-	private Vector3 getDirection() {
-		float orientation = super.bodies.get(0).getOrientation().getAngle()+270f;
-		return new Vector3(Vector3.X).rotate(Vector3.Y, orientation);
+	private float getDirection() {
+		return super.bodies.get(0).getOrientation().getAngle();
 	}
 	
 	private void updateTicks() {
-		Vector3 lastL = lastCod[0].cpy();
-		Vector3 lastR = lastCod[1].cpy();
-		updatePositions();
-		Vector3 direction = getDirection();
-		float deltaTickLeft = lastL.dst2(lastCod[0]);
-		float deltaTickRight = lastR.dst2(lastCod[1]);
-		boolean invertDirLeft = direction.hasSameDirection(lastCod[0].cpy().sub(lastL));
-		boolean invertDirRight = direction.hasSameDirection(lastCod[1].cpy().sub(lastR));
-		tick[0] += (invertDirLeft ? 1f : -1f) * deltaTickLeft*10f;
-		tick[1] += (invertDirRight ? 1f : -1f) * deltaTickRight*10f;
+		//updatePositions();
+		tick = (consigne < 0 ? -1f : 1f) * super.bodies.get(0).getCenterOfMassPosition().dst(lastPosition);
 	}
 	
 	public void resetAll() {
-		for (int i = 0; i < commandeCod.length; i++) {
-			tick[i] = 0f;
-			sumErrorCod[i] = 0f;
-			commandeCod[i] = 0f;
-			lastErrorCod[i] = errorCod[i];
-		}
+		tick = 0f;
+		sumErrorCod = 0f;
+		commandeCod = 0f;
+		lastErrorCod = errorCod;
+		updatePositions();
 	}
 	
 	public boolean asserv() {
@@ -111,19 +90,28 @@ public class Robot extends BulletObject{
 		 **/
 		if(stable >= asservStable) {
 			resetAll();
+			move(Vector3.Zero);
 			return true;
 		}
-		for (int i = 0; i < commandeCod.length; i++) {
-			float error = consigne[i] - tick[i];
-			errorCod[i] = (error < erreurAdmissible && error > erreurAdmissible) ? 0f : error;
-			sumErrorCod[i] += errorCod[i];
-			commandeCod[i] = Kp * errorCod[i] + Ki * sumErrorCod[i] + Kd * (errorCod[i] - lastErrorCod[i]);
-			lastErrorCod[i] = errorCod[i];
-		}
-		if(Math.abs(errorCod[0]) <= 0.2f && Math.abs(errorCod[1]) <= 0.2f) {
+		float error = consigne - tick;
+		errorCod = (error < erreurAdmissible && error > erreurAdmissible) ? 0f : error;
+		sumErrorCod += errorCod;
+		commandeCod = Kp * errorCod + Ki * sumErrorCod + Kd * (errorCod - lastErrorCod);
+		lastErrorCod = errorCod;
+		if(Math.abs(errorCod) <= 0.2f) {
 			stable += 1;
 		}
 		return false;
+	}
+	
+	private void move(Vector3 nextPosition) {
+		if(moveit.len2() <= 10) {
+			super.bodies.get(0).setAngularVelocity(Vector3.Zero);
+			super.bodies.get(0).setLinearVelocity(Vector3.Zero);
+		}else {
+			super.bodies.get(0).applyCentralImpulse(moveit);
+		}
+		super.bodies.get(0).setLinearVelocity(super.bodies.get(0).getLinearVelocity().limit2(1000.0f));
 	}
 	
 	@Override
@@ -139,16 +127,15 @@ public class Robot extends BulletObject{
 		}else {
 			initFrames--;
 			updateTicks();
-			if(initFrames%5==0) {
-				//super.bodies.get(0).setAngularVelocity(Vector3.Zero);
-				//super.bodies.get(0).setLinearVelocity(Vector3.Zero);
-				asserv();
-				for (int i = 0; i < 2; i++) {
-					moveit = new Vector3(commandeCod[i], 0f, 0f);
-					moveit.rotate(Vector3.Y, super.bodies.get(0).getOrientation().getAngle()+270f);
-					super.bodies.get(0).applyForce(moveit, lastCod[i].cpy().sub(lastPosition));
+			if(true || initFrames%5==0) {
+				if(asserv()) {
+					consigne = consigne == 30f ? -30f : 30f;
+					stable = 0;
+					System.out.println(consigne);
 				}
-				System.out.println(lastErrorCod[0]);
+				moveit = new Vector3(commandeCod, 0f, 0f);
+				moveit.rotate(Vector3.Y, getDirection());
+				move(moveit);
 			}
 		}
 	}
