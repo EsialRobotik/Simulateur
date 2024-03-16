c6dfeda5b7f5a82a9bcc8285476b03262e501ec5
GaG <francois.prugniel@telecomnancy.net>
2024-03-16 23:02:33 +0100

    2024

--- javascript/currentYear.js
+++ javascript/currentYear.js
@@ -1,6 +1,7 @@
-var currentYear = 2023;
-var rotateTable = true;
+var currentYear = 2024;
+var rotateTable = false;
 
 var bigRobot = `../resources/${currentYear}/bigPrincess.png`;
-var smallRobot = `../resources/${currentYear}/pmiPrincess.png`;
+//var smallRobot = `../resources/${currentYear}/pmiPrincess.png`;
+var smallRobot = null;
 var table = `../resources/${currentYear}/table.svg`;
